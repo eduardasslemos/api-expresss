82d61fe2590fd1e6f87e24cba8fc4339bddf995e
eduardasslemos <eduardasslemos@gmail.com>
2026-05-10 17:20:53 -0300

    final commit

--- dist/app.js
+++ dist/app.js
@@ -1,35 +1,30 @@
-import { Produto } from "./Produto"
-import express, { Request, Response } from "express"
-
-const app = express()
-const PORT = process.env.PORT ?? 3000
-const produtos: Produto[] = [];
-app.use(express.json())
-
-function novoProduto (req: Request, res: Response): void {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Produto_1 = require("./Produto");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT ?? 3000;
+const produtos = [];
+app.use(express_1.default.json());
+function novoProduto(req, res) {
     try {
-        let data: any = req.body;
-
-        if (
-            !data.nome ||
+        let data = req.body;
+        if (!data.nome ||
             !data.preco ||
-            !data.fabricante
-        ) {
-            throw new Error (
-            "Produto requer nome, preco e fabricante"
-            );
+            !data.fabricante) {
+            throw new Error("Produto requer nome, preco e fabricante");
         }
-        
         //validacao para nao ter id duplicado
         const produtoExistente = produtos.find(produto => produto.id === data.id);
-
         if (produtoExistente) {
             res.status(400).json({
                 Message: "Já existe um produto com esse ID"
             });
             return;
         }
-
         //validacao para o preco ser maior que 0
         if (data.preco <= 0) {
             res.status(400).json({
@@ -37,19 +32,15 @@ function novoProduto (req: Request, res: Response): void {
             });
             return;
         }
-
         //validacao para que o endereco seja preenchido
-        if (
-            !data.fabricante.endereco ||
+        if (!data.fabricante.endereco ||
             !data.fabricante.endereco.cidade ||
-            !data.fabricante.endereco.pais
-        ) {
+            !data.fabricante.endereco.pais) {
             res.status(400).json({
                 Message: "Cidade e pais sao obrigatorios"
             });
             return;
         }
-
         //validacao para que o fabricante tenha nome
         if (!data.fabricante.nome) {
             res.status(400).json({
@@ -57,83 +48,62 @@ function novoProduto (req: Request, res: Response): void {
             });
             return;
         }
-
-        const produto = new Produto(
-        data.id ,
-        data.nome ,
-        data.preco ,
-        data.fabricante
-        );
-
+        const produto = new Produto_1.Produto(data.id, data.nome, data.preco, data.fabricante);
         produtos.push(produto);
-
         res.status(200).json(produto);
-    } catch (e: unknown) {
+    }
+    catch (e) {
         res.status(500).json({
-            Message: (e as Error).message
+            Message: e.message
         });
     }
 }
-
-function buscarProduto(req: Request, res: Response): void{
-    try{
+function buscarProduto(req, res) {
+    try {
         let id = Number(req.params.id);
-
         if (isNaN(id)) {
             res.status(400).json({
                 Message: "ID inválido"
             });
             return;
         }
-
         const produto = produtos.find(produto => produto.id === id);
-
         if (!produto) {
             res.status(404).json({
                 Message: "Produto não encontrado"
             });
             return;
         }
-
         res.status(200).json(produto);
-    } catch (e: unknown) {
+    }
+    catch (e) {
         res.status(500).json({
-            Message: (e as Error).message
+            Message: e.message
         });
     }
 }
-
-function atualizarProduto(req: Request, res: Response): void{
-    try{
-        let data: any = req.body;
+function atualizarProduto(req, res) {
+    try {
+        let data = req.body;
         let id = Number(req.params.id);
-
         if (isNaN(id)) {
             res.status(400).json({
                 Message: "ID inválido"
             });
             return;
         }
-
         const produto = produtos.find(produto => produto.id === id);
-
         if (!produto) {
             res.status(404).json({
                 Message: "Produto não encontrado"
             });
             return;
         }
-
-        if (
-            !data.nome ||
+        if (!data.nome ||
             !data.preco ||
-            !data.fabricante
-        ) {
-            throw new Error (
-            "Produto requer nome, preco e fabricante"
-            ) ;
+            !data.fabricante) {
+            throw new Error("Produto requer nome, preco e fabricante");
         }
-
         //validacao para o preco ser maior que 0
         if (data.preco <= 0) {
             res.status(400).json({
@@ -141,19 +111,15 @@ function atualizarProduto(req: Request, res: Response): void{
             });
             return;
         }
-
         //validacao para que o endereco seja preenchido
-        if (
-            !data.fabricante.endereco ||
+        if (!data.fabricante.endereco ||
             !data.fabricante.endereco.cidade ||
-            !data.fabricante.endereco.pais
-        ) {
+            !data.fabricante.endereco.pais) {
             res.status(400).json({
                 Message: "Cidade e pais sao obrigatorios"
             });
             return;
         }
-
         //validacao para que o fabricante tenha nome
         if (!data.fabricante.nome) {
             res.status(400).json({
@@ -161,53 +127,45 @@ function atualizarProduto(req: Request, res: Response): void{
             });
             return;
         }
-
         produto.nome = data.nome;
         produto.preco = data.preco;
         produto.fabricante = data.fabricante;
-
         res.status(200).json(produto);
-    } catch (e: unknown) {
+    }
+    catch (e) {
         res.status(500).json({
-            Message: (e as Error).message
+            Message: e.message
         });
     }
 }
-
-function removerProduto(req: Request, res: Response): void{
-    try{
+function removerProduto(req, res) {
+    try {
         let id = Number(req.params.id);
-        
         if (isNaN(id)) {
             res.status(400).json({
                 Message: "ID inválido"
             });
             return;
         }
-
         const produto = produtos.find(produto => produto.id === id);
-
         if (!produto) {
             res.status(404).json({
                 Message: "Produto não encontrado"
             });
             return;
         }
-
         const index = produtos.indexOf(produto);
         produtos.splice(index, 1);
-
         res.status(200).json(produto);
-    } catch (e: unknown) {
+    }
+    catch (e) {
         res.status(500).json({
-            Message: (e as Error).message
+            Message: e.message
         });
     }
 }
-
 app.post('/api/product', novoProduto);
 app.get('/api/product/:id', buscarProduto);
 app.put('/api/product/:id', atualizarProduto);
 app.delete('/api/product/:id', removerProduto);
-
-app.listen(PORT, () => console.log(`API em execução no URL: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`API em execução no URL: http://localhost:${PORT}`));
